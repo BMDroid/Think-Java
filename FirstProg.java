44977f78e20221e2578c247efc4d182a5b740b42
BMDroid <ametrojan@gmail.com>
2020-12-07 15:19:09 +0800

    first

---
+++ FirstProg.java
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Author: BO Jianyuan
+ * @Date: 2020-12-07 15:00:43
+ * @LastEditors: BO Jianyuan
+ * @LastEditTime: 2020-12-07 15:16:41
+ */
+import java.time.format.DateTimeFormatter; 
+import java.time.LocalDateTime;    
+
+ public class FirstProg{
+     public static void main(String[] args){
+         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");  
+         LocalDateTime now = LocalDateTime.now(); 
+         System.out.println("I am determined to learn how to code.");
+         System.out.println(String.format("Today's date is %s.", dtf.format(now)));
+     }
+ }
